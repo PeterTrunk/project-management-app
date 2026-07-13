1a78ae21a20bbd37abbe2949ce54128ca3fcaa1d
PeterTrunk <trunkpeter9@gmail.com>
2026-07-13 21:53:09 +0200

    debug: Add more debug writeline to program.cs

--- backend/src/ProjectManager.API/Program.cs
+++ backend/src/ProjectManager.API/Program.cs
@@ -123,7 +123,7 @@ builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
     });
 
 var redisConnection = Environment.GetEnvironmentVariable("REDIS_CONNECTION");
-
+Console.WriteLine("#Debug 0");
 var signalRBuilder = builder.Services.AddSignalR();
 if (!string.IsNullOrEmpty(redisConnection))
 {
@@ -133,6 +133,7 @@ if (!string.IsNullOrEmpty(redisConnection))
     });
     Console.WriteLine("#Redis-el fut!");
 }
+Console.WriteLine("#Debug 1");
 
 builder.Services.AddSwaggerGen(options =>
 {
